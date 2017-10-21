c50575d962bf348948b02bd567c49ed1919a2ba3
Gener Angelo Lopez <gener.lopez@obf.ateneo.edu>
2017-10-21 23:44:08 +0800

    Initial commit, nonfunctional login page but it's the thought that counts

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import Application from './pages/Application';
+import store from './redux';
+ 
+export default class Gofer extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Application />
+      </Provider>
+    );
+  }
+}
